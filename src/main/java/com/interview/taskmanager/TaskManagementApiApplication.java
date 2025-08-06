13bdde8583d1b8381acfe4a70cd31504e0624fdc
reubenkl <reubenkl1602@gmail.com>
2025-08-06 13:59:22 +0530

    Refactoring file names

--- src/main/java/com/interview/taskmanager/TaskManagementApiApplication.java
+++ src/main/java/com/interview/taskmanager/TaskManagementApiApplication.java
@@ -1,4 +1,4 @@
-package com.reuben.interview;
+package com.interview.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/interview/taskmanager/TaskManagementApiApplicationTests.java
+++ src/test/java/com/interview/taskmanager/TaskManagementApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.reuben.interview;
+package com.interview.taskmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
